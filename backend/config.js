d0efddbb29117975b4376844f5c0b487b3e2bb7b
ilyas <ilyas@sjainfosolutions.com>
2020-08-24 15:03:37 +0530

    Connect with Cloud Mongo Atlas DB

--- backend/config.js
+++ backend/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 export default {
   PORT: process.env.PORT || 5000,
-  MONGODB_URL: process.env.MONGODB_URL || 'mongodb://localhost/book_store',
+  MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://5Vi0zTnVsxVgnXVS:5Vi0zTnVsxVgnXVS@cluster0.lj5e9.mongodb.net/book_store?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
   accessKeyId: process.env.accessKeyId || 'accessKeyId',
