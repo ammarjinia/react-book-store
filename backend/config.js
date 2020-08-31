8ff5d26c77cc05149e7db7de9c483b7359ec4d64
ilyas <ilyas@sjainfosolutions.com>
2020-08-31 20:39:03 +0530

    Update AWS

--- backend/config.js
+++ backend/config.js
@@ -7,6 +7,6 @@ export default {
   MONGODB_URL: process.env.MONGODB_URL || 'mongodb+srv://5Vi0zTnVsxVgnXVS:5Vi0zTnVsxVgnXVS@cluster0.lj5e9.mongodb.net/book_store?retryWrites=true&w=majority',
   JWT_SECRET: process.env.JWT_SECRET || 'somethingsecret',
   PAYPAL_CLIENT_ID: process.env.PAYPAL_CLIENT_ID || 'sb',
-  accessKeyId: process.env.accessKeyId || 'AKIAIZ4ZXTBFOPMTCZJA',
-  secretAccessKey: process.env.secretAccessKey || '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',
+  accessKeyId: process.env.accessKeyId || 'AKIAJNP7VMAEZ666DQ3Q',
+  secretAccessKey: process.env.secretAccessKey || ' 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',
 };
